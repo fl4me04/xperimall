b05e86c45b6dbd92012982023e32b538cded09e8
Midevery <152509883+Midevery@users.noreply.github.com>
2025-05-16 22:24:53 +0700

    Update main.go

--- XperimallBackend/main.go
+++ XperimallBackend/main.go
@@ -4,18 +4,24 @@ import (
 	"XperimallBackend/database"
 	"XperimallBackend/routes"
 
+	"os"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    database.ConnectDB()
+	database.ConnectDB()
 
-    r.Use(cors.Default())
+	r.Use(cors.Default())
 
-    routes.SetupRoutes(r)
+	routes.SetupRoutes(r)
 
-    r.Run(":8080")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	r.Run(":" + port)
 }
