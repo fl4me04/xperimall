ce85ae610dca9720d3a202f711004c03f8d3d578
ELROY HANS ANGGADA <elroyhans@gmail.com>
2025-05-29 08:24:40 +0700

    Done promo page and promo detail backend

--- XperimallBackend/routes/routes.go
+++ XperimallBackend/routes/routes.go
@@ -23,6 +23,7 @@ func SetupRoutes(r *gin.Engine) {
 		api.GET("/floors", controllers.GetFloors)
 		api.GET("/floors/:floorId/activities", controllers.GetActivitiesByFloor)
 		api.GET("/tenants/:id", controllers.GetTenantByID)
+		api.GET("/promotions/:id", controllers.GetPromotionByID)
 	}
 
 	expenses := r.Group("/expenses")
